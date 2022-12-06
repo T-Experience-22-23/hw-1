8193f164efb3680cd10a05c846881240b259e4bc
Erez Levanon <erezlevanon@users.noreply.github.com>
2022-12-06 17:56:08 +0200

    rotating students

--- app.js
+++ app.js
@@ -136,44 +136,10 @@ function htmlSeparateDiff(base, text) {
 }
 
 class MainController {
-  constructor($window, $sce) {
+  constructor($window, $sce, $http) {
     this.window_ = $window;
     this.test = '';
     this.base = '';
-
-    let test = `
-        let t = 0; // time variable
-        
-        function setup() {
-          createCanvas(740, 725);
-          noStroke;
-          //or stroke (1, 0, 0)
-          fill(180, 0, 0);
-        }
-        
-        function draw() {
-          background(10, 10); // translucent background (creates trails)
-        
-          // make a x and y grid of ellipses
-          for (let x = 0; x <= width; x = x + 30) {
-            for (let y = 0; y <= height; y = y + 30) {
-              // starting point of each circle depends on mouse position
-              const xAngle = map(mouseX, 0, width, 4 * PI, -4 * PI, true);
-              const yAngle = map(mouseY, 0, height, 4 * PI, -4 * PI, true);
-              // and also varies based on the particle's location
-              const angle = xAngle * (x / width) + yAngle * (y / height);
-        
-              // each particle moves in a circle
-              const myX = x + 300 * cos(3 * PI * t + angle);
-              const myY = y + 30 * sin(30 * PI * t + angle);
-        
-              triangle(myX, 350, 380, myY, 300, 100);// draw particle
-            }
-          }
-        
-          t = t + 0.001; // update time
-        }
-        `;
 
     this.students = STUDENTS.map((v) => {
       const diff_sides = htmlSeparateDiff(BASE, test);
@@ -186,6 +152,21 @@ class MainController {
         test: diff_sides[1],
       };
     });
+    this.studentIndex = 0;
+    this.$http.get('http://editor.p5js.org/mayauni13/sketches/xiSSlif93').then((v) => void console.log(v));;
+    this.$http.get('https://editor.p5js.org/mayauni13/sketches/xiSSlif93').then((v) => void console.log(v));;
+  }
+
+  incrementStudent() {
+    this.studentIndex = (this.studentIndex + 1) % this.students.length;
+  }
+
+  decrementStudent() {
+    this.studentIndex = (this.studentIndex - 1) % this.students.length;
+  }
+  
+  get student() {
+    return this.students[this.studentIndex];
   }
 }
 
